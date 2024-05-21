7c34eac3591f41ca04c89a3be2cdfb26d3b16be9
SSProgamer <72447877+SSProgamer@users.noreply.github.com>
2024-05-21 12:05:12 +0700

    Add player rank to game database when create room.

--- public/createroom.js
+++ public/createroom.js
@@ -1,29 +1,39 @@
-
 function createRoom(publceRoom) {
     const roomCode = generateRoomCode();
     const roomURL = window.location.href + '?room=' + roomCode; // create code room
     console.log(roomURL);
     const createRoom = firebase.database().ref("Game")
     const currentUser = firebase.auth().currentUser;
+    const getUserData = firebase.database().ref("user_google");
     let tmpTD = `user-x-id`;
     let tmpEmail = `user-x-email`;
     let tmpName = `user-x-name`;
     let tmpGenrank = `user-x-genrank`;
-
-    createRoom.child(roomCode).update({
-        start: "start",
-        [tmpTD]: currentUser.uid,
-        [tmpEmail]: currentUser.email,
-        matchmaking: publceRoom,
-        [tmpName]: currentUser.displayName,
-        [tmpGenrank]: 1
+    
+    getUserData.child(currentUser.uid).once("value").then((snapshot) => {
+        let playerGenrank = 1;
+        snapshot.forEach((data) => {
+            let id = data.key;
+            let id_data = data.val();
+            if (id == "generalrank") {
+                playerGenrank = id_data;
+            }
+        });
+        createRoom.child(roomCode).update({
+            start: "start",
+            [tmpTD]: currentUser.uid,
+            [tmpEmail]: currentUser.email,
+            matchmaking: publceRoom,
+            [tmpName]: currentUser.displayName,
+            [tmpGenrank]: playerGenrank
+        });
+        addUser.child(currentUser.uid).update({
+            Isplay: true,
+            Inroom: roomCode,
+        })
+    
+        window.location.href = `game.html?room=${roomCode}`;
     });
-    addUser.child(currentUser.uid).update({
-      Isplay: true,
-      Inroom: roomCode,
-    })
-
-    window.location.href = `game.html?room=${roomCode}`;
 }
 
 function generateRoomCode() {
